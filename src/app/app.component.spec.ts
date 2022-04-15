1a3397cc7b09011884b5cf1560970771009a2638
Patrick Bußmann <patrick.bussmann@bussmann-it.de>
2022-04-15 23:51:33 +0200

    Added problem test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
 describe('AppComponent', () => {
+  let httpTestingController: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -27,5 +32,16 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tour-of-heroes app is running!');
+  });
+
+  it('should execute http request', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const response = fixture.componentInstance.executeHttpRequest();
+
+    const req = httpTestingController.expectOne('http://localhost');
+    expect(req.request.method).toEqual('GET');
+    req.flush('Ok this works');
+
+    expect(await response).toEqual('Ok this works');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  async executeHttpRequest() {
+    await Promise.resolve('ok'); // Results in a problem
+
+    return new Promise((resolve, reject) => {
+      this.http.get('http://localhost').subscribe({
+        next: (value) => {
+          resolve(value);
+        },
+        error: (error) => {
+          reject(error);
+        }
+      });
+    });
+  }
 }
